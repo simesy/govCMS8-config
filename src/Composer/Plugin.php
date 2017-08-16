ea296ba852eae03207d4e779486889908ce77876
Joseph Zhao <joseph.zhao@xing.net.au>
2017-08-16 18:37:25 +1000

    Update the plugin placeholder

--- src/Composer/Plugin.php
+++ src/Composer/Plugin.php
@@ -5,9 +5,8 @@ namespace govCMS\Config\Composer;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\EventDispatcher\EventSubscriberInterface;
 
-class Plugin implements PluginInterface, EventSubscriberInterface
+class Plugin implements PluginInterface
 {
 
     /**
