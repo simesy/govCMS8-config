5b9e5ff6fc6516da553a4f58fcece26f686df71f
Joseph Zhao <joseph.zhao@xing.net.au>
2017-08-27 16:52:40 +1000

    Update plugin

--- src/Composer/Plugin.php
+++ src/Composer/Plugin.php
@@ -5,6 +5,8 @@ namespace govCMS\Config\Composer;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Util\Filesystem;
 
 class Plugin implements PluginInterface
 {
@@ -29,6 +31,45 @@ class Plugin implements PluginInterface
     {
         $this->composer = $composer;
         $this->io = $io;
+    }
+
+    /**
+     * Creates or updates composer include files.
+     *
+     * @param \Composer\Script\Event $event
+     */
+    public function scaffoldComposerIncludes(Event $event)
+    {
+        $files = [
+            'composer.config.json',
+        ];
+    }
+
+    /**
+     * Get the path to the 'vendor' directory.
+     *
+     * @return string
+     *   The file path of the 'vendor' directory.
+     */
+    public function getVendorPath()
+    {
+        $config = $this->composer->getConfig();
+        $filesystem = new Filesystem();
+        $filesystem->ensureDirectoryExists($config->get('vendor-dir'));
+        $vendorPath = $filesystem->normalizePath(realpath($config->get('vendor-dir')));
+
+        return $vendorPath;
+    }
+
+    /**
+     * Returns the repo root's path, assumed to be one dir above vendor dir.
+     *
+     * @return string
+     *   The file path of the repository root.
+     */
+    public function getRepoRoot()
+    {
+        return dirname($this->getVendorPath());
     }
 
 }
