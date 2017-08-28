fb51491e7c899718346d6e27c825648dc309c455
Joseph Zhao <joseph.zhao@xing.net.au>
2017-08-28 11:05:54 +1000

    Update composer plugin codes

--- src/Composer/Plugin.php
+++ src/Composer/Plugin.php
@@ -5,10 +5,17 @@ namespace govCMS\Config\Composer;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvents;
+use Composer\Installer\PackageEvent;
+use Composer\Script\ScriptEvents;
 use Composer\Script\Event;
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\UpdateOperation;
+use Composer\Util\ProcessExecutor;
 use Composer\Util\Filesystem;
 
-class Plugin implements PluginInterface
+class Plugin implements PluginInterface, EventSubscriberInterface
 {
 
     /**
@@ -22,6 +29,26 @@ class Plugin implements PluginInterface
     protected $io;
 
     /**
+     * @var
+     */
+    protected $eventDispatcher;
+
+    /**
+     * @var ProcessExecutor
+     */
+    protected $executor;
+
+    /**
+     * @var string
+     */
+    protected $govCMSConfigPackageName = 'govcms/govcms8-config';
+
+    /**
+     * @var PackageInterface
+     */
+    protected $govCMSConfigPackage;
+
+    /**
      * Activate function.
      *
      * @param Composer $composer
@@ -31,6 +58,74 @@ class Plugin implements PluginInterface
     {
         $this->composer = $composer;
         $this->io = $io;
+        $this->eventDispatcher = $composer->getEventDispatcher();
+        // Set timeout 3000.
+        ProcessExecutor::setTimeout(3000);
+        $this->executor = new ProcessExecutor($this->io);
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            PackageEvents::POST_PACKAGE_INSTALL => "onPostPackageEvent",
+            PackageEvents::POST_PACKAGE_UPDATE => "onPostPackageEvent",
+            ScriptEvents::POST_UPDATE_CMD => 'onPostCmdEvent',
+        ];
+    }
+
+    /**
+     * Execute update after update command has been executed, if applicable.
+     *
+     * @param PackageEvent $event
+     */
+    public function onPostPackageEvent(PackageEvent $event)
+    {
+        $package = $this->getGovCMSPackage($event->getOperation());
+        if ($package) {
+            $this->govCMSConfigPackage = $package;
+        }
+    }
+
+    /**
+     * Post update after command has been executed, if applicable.
+     *
+     * @param Event $event
+     */
+    public function onPostCmdEvent(Event $event)
+    {
+        // Only install the template files if govCMS package was installed.
+        if (isset($this->govCMSConfigPackage)) {
+            $version = $this->govCMSConfigPackage->getVersion();
+            $this->executeUpdate($version);
+        }
+    }
+
+    /**
+     * @param $operation
+     *
+     * @return null
+     */
+    protected function getGovCMSPackage($operation)
+    {
+        if ($operation instanceof InstallOperation) {
+            $package = $operation->getPackage();
+        } elseif ($operation instanceof UpdateOperation) {
+            $package = $operation->getTargetPackage();
+        }
+        if (isset($package) && $package instanceof PackageInterface && $package->getName() == $this->govCMSConfigPackageName) {
+            return $package;
+        }
+        return null;
+    }
+    /**
+     * @param $version
+     */
+    protected function executeUpdate($version)
+    {
+        $this->io->write('<comment>Skipping update of templated files</comment>');
     }
 
     /**
