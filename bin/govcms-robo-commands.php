237e2a289a8ff1c6ba43c69ad33067b84d91a771
Joseph Zhao <joseph.zhao@xing.net.au>
2017-08-28 14:19:30 +1000

    Robo as a Framework for govCMS build process

--- bin/govcms-robo-commands.php
+++ bin/govcms-robo-commands.php
@@ -5,9 +5,13 @@
  * govCMS Robo commands.
  */
 
-use Robo\Common\TimeKeeper;
-use Symfony\Component\Console\Input\ArgvInput;
-use Symfony\Component\Console\Output\ConsoleOutput;
+ use Robo\Common\TimeKeeper;
+ use Symfony\Component\Console\Input\ArgvInput;
+ use Symfony\Component\Console\Output\ConsoleOutput;
+ use Robo\Config\Config;
+ use Consolidation\Config\Loader\YamlConfigLoader;
+ use Consolidation\Config\Loader\ConfigProcessor;
+ use govCMS\Config\Robo\Entry;
 
 // Start Timer.
 $timer = new TimeKeeper();
@@ -17,8 +21,18 @@ $timer->start();
 $input = new ArgvInput($_SERVER['argv']);
 $output = new ConsoleOutput();
 
+// Initialize configurations.
+$config = new Config();
+$loader = new YamlConfigLoader();
+$processor = new ConfigProcessor();
+$processor->extend($loader->load('govcms.build.yml'));
+$config->import($processor->export());
+
+$app = new \Entry($config, $input, $output);
+$status_code = $app->run($input, $output);
+
 // Stop timer.
 $timer->stop();
 $output->writeln("<comment>" . $timer->formatDuration($timer->elapsed()) . "</comment> total time elapsed.");
 
-exit();
+exit($status_code);
---
+++ src/Robo/Entry.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace govCMS\Config\Robo;
+
+use Robo\Common\ConfigAwareTrait;
+use Robo\Config;
+use Robo\Robo;
+use Robo\Runner as RoboRunner;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Entry implements ContainerAwareInterface, LoggerAwareInterface
+{
+
+
+
+}
