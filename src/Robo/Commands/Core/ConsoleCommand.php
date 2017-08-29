467c5d23acce8876ad6c6c3ea17c9b0936638982
Joseph Zhao <joseph.zhao@xing.net.au>
2017-08-29 14:56:17 +1000

    Update ConsoleCommand class

--- src/Robo/Commands/Core/ConsoleCommand.php
+++ src/Robo/Commands/Core/ConsoleCommand.php
@@ -23,4 +23,60 @@ class ConsoleCommand extends CommandBase
 
     }
 
+    /**
+     * Find the position of a closing bracket for a given stanza in a string.
+     *
+     * @param $contents
+     *   The string containing the brackets.
+     * @param int $start_pos
+     *   The position of the opening bracket in the string that should be
+     *   matched.
+     *
+     * @return int|null
+     */
+    protected function getClosingBracketPosition($contents, $start_pos)
+    {
+        $brackets = ['{'];
+        for ($pos = $start_pos; $pos < strlen($contents); $pos++) {
+            $char = substr($contents, $pos, 1);
+            if ($char == '{') {
+                array_push($brackets, $char);
+            } elseif ($char == '}') {
+                array_pop($brackets);
+            }
+            if (count($brackets) == 0) {
+                return $pos;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Creates a ~/.bash_profile on OSX if one does not exist.
+     */
+    protected function createOsxBashProfile()
+    {
+        if ($this->getInspector()->isOsx()) {
+            $continue = $this->confirm("Would you like to create ~/.bash_profile?");
+            if ($continue) {
+                $user = posix_getpwuid(posix_getuid());
+                $home_dir = $user['dir'];
+                $bash_profile = $home_dir.'/.bash_profile';
+                if (!file_exists($bash_profile)) {
+                    $result = $this->taskFilesystemStack()
+                      ->touch($bash_profile)
+                      ->run();
+                    if (!$result->wasSuccessful()) {
+                        throw new \Exception("Could not create $bash_profile.");
+                    }
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
 }
